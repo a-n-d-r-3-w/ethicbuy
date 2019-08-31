5f984391ddc14867103acbe56cbe86d6200f519f
andr3w <liu.anray+github@gmail.com>
2019-08-30 23:56:05 -0400

    Retrieve B corporations

--- bcorporation.js
+++ bcorporation.js
@@ -1,8 +1,29 @@
 const fetch = require('isomorphic-unfetch');
+const cheerio = require('cheerio')
+
+const bcorporations = new Set();
 
 const collect = async () => {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  console.log(res);
+  let pageNum = 0;
+  while (true) {
+    console.log('pageNum: ', pageNum);
+    const res = await fetch(`https://bcorporation.net/directory?page=${pageNum}`);
+    const html = await res.text();
+    const $ = cheerio.load(html);
+    const cardTitles = $('.card__title');
+    if (cardTitles.length === 0) {
+      break;
+    }
+    cardTitles.each(function (index) {
+      const bcorporation = $(this).html();
+      console.log(bcorporation);
+      bcorporations.add(bcorporation);
+    })
+    pageNum += 1;
+  }
 };
 
-collect();
+
+collect().then(() => {
+  console.log('Number of B corporations: ', bcorporations.size);  
+});
