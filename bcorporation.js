cae6b960b9821bdcba89fd4ac3b2f62e36717729
andr3w <liu.anray+github@gmail.com>
2019-09-03 23:04:57 -0400

    Get full corporation names instead of truncated names

--- bcorporation.js
+++ bcorporation.js
@@ -1,31 +1,40 @@
 const fetch = require('isomorphic-unfetch');
 const cheerio = require('cheerio')
 
-const bcorporations = new Set();
-
 const collect = async () => {
+  const bcorporations = new Set();
+
   let pageNum = 0;
   while (true) {
-    console.log('pageNum: ', pageNum);
-    const res = await fetch(`https://bcorporation.net/directory?page=${pageNum}`);
+    let url = 'https://bcorporation.net/directory';
+    if (pageNum > 0) {
+      url += `?page=${pageNum}`;
+    }
+
+    const res = await fetch(url);
     const html = await res.text();
+
     const $ = cheerio.load(html);
-    const cardTitles = $('.card__title');
-    if (cardTitles.length === 0) {
+    const cardImages = $('.card img');
+    if (cardImages.length === 0) {
       break;
     }
-    cardTitles.each(function (index) {
-      const bcorporation = $(this).html();
+
+    cardImages.each(function () {
+      const bcorporation = $(this).attr('title');
       console.log(bcorporation);
       bcorporations.add(bcorporation);
     })
+
     pageNum += 1;
   }
+
+  return bcorporations;
 };
 
 
 // TODO: Update data on a regular basis.
-collect().then(() => {
+collect().then(bcorporations => {
   // TODO: Store data in a file.
   console.log('Number of B corporations: ', bcorporations.size);
 });
